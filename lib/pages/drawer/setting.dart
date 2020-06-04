38229e00bc7570e59ec12a9d5be3f73f2ebb8c66
chaves <mail@chavesgu.com>
2020-06-04 15:16:23 +0800

    add animate list demo

--- lib/pages/drawer/setting.dart
+++ lib/pages/drawer/setting.dart
@@ -18,87 +18,150 @@ class SettingPage extends StatefulWidget {
 
 class SettingPageState extends State<SettingPage> {
 
+  GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
+
+  List<int> listControl = [];
+
+  List<Widget> widgetList = [];
+
   @override
   Widget build(BuildContext context) {
     final isDark = Provider.of<GlobalModel>(context, listen: true).isDark;
     final setTheme = Provider.of<GlobalModel>(context, listen: false).changeTheme;
     final toggleUseSystemMode = Provider.of<GlobalModel>(context, listen: false).toggleUseSystemMode;
     final toggleAppThemeMode = Provider.of<GlobalModel>(context, listen: false).toggleAppThemeMode;
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50),
         child: AppBar(
-          centerTitle: true,
           title: Text('设置'),
         ),
       ),
       body: Consumer<GlobalModel>(
         builder: (context, GlobalModel model, child) {
-          return ListView(
+          widgetList = _buildWidgetList(model);
+          return AnimatedList(
+            key: listKey,
+            initialItemCount: listControl.length,
+            primary: true,
             physics: BouncingScrollPhysics(),
-            children: <Widget>[
-              _renderItem(
-                title: Text('跟随系统主题'),
-                control: CupertinoSwitch(
-                  value: model.useSystemMode,
-                  onChanged: (isOn) {
-                    toggleUseSystemMode(isOn);
-                  },
-                ),
-                isDark: model.isDark,
-              ),
-              Offstage(
-                offstage: model.useSystemMode,
-                child: AnimatedOpacity(
-                  opacity: model.useSystemMode?0:1,
-                  duration: Duration(milliseconds: 200),
-                  child: _renderItem(
-                    title: Text('深色主题'),
-                    control: CupertinoSwitch(
-                      value: model.appThemeMode!=ThemeMode.light,
-                      onChanged: (isOn) {
-                        toggleAppThemeMode(isOn?ThemeMode.dark:ThemeMode.light);
-                      },
-                    ),
-                    isDark: model.isDark,
-                  ),
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  ...(isDark?darkThemeList:themeList).map((Color color) {
-                    return GestureDetector(
-                      child: Container(
-                        width: 40,
-                        height: 40,
-                        margin: EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: isDark?Colors.white:Colors.black),
-                          color: color
-                        ),
-                      ),
-                      onTap: () {
-                        setTheme((isDark?darkThemeList:themeList).indexOf(color));
-                      },
-                    );
-                  }).toList(),
-                ],
-              )
-            ],
+            itemBuilder: (BuildContext context, int index, Animation<double> animation) {
+              return SizeTransition(
+                sizeFactor: animation,
+                child: widgetList[listControl[index]],
+              );
+            },
           );
         },
       ),
     );
   }
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    GlobalModel model = context.read<GlobalModel>();
+    listControl.add(0);
+    if (!model.useSystemMode) listControl.add(1);
+    listControl.add(2);
+  }
 
-  Widget _renderItem({ Widget title, Widget control, bool isDark = false}) {
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    // below toggle AnimateList 、 useSystemMode
+    GlobalModel model = context.read<GlobalModel>();
+    bool useSystemMode = model.useSystemMode;
+    print('是否使用系统主题: $useSystemMode');
+    if (!useSystemMode) {
+      int index = listControl.indexOf(1);
+      if (index < 0) {
+        listControl.insert(1, 1);
+        listKey.currentState?.insertItem(1, duration: Duration(milliseconds: 200));
+      }
+    } else {
+      int index = listControl.indexOf(1);
+      if (index>=0) {
+        listControl.removeAt(index);
+        listKey.currentState?.removeItem(index, (BuildContext context, Animation<double> animation){
+          return SizeTransition(
+            sizeFactor: animation,
+            child: _removedItem(model),
+          );
+        }, duration: Duration(milliseconds: 200));
+      }
+    }
+  }
+
+  Widget _removedItem(GlobalModel model) {
+    return _renderItem(
+      title: Text('深色主题'),
+      control: CupertinoSwitch(
+        value: model.appThemeMode!=ThemeMode.light,
+        onChanged: (isOn) {
+          model.toggleAppThemeMode(isOn?ThemeMode.dark:ThemeMode.light);
+        },
+      ),
+      model: model,
+    );
+  }
+
+  List<Widget> _buildWidgetList(GlobalModel model) {
+    return [
+      _renderItem(
+        title: Text('跟随系统主题'),
+        control: CupertinoSwitch(
+          value: model.useSystemMode,
+          onChanged: (isOn) {
+            model.toggleUseSystemMode(isOn);
+          },
+        ),
+        model: model,
+      ),
+      _renderItem(
+        title: Text('深色主题'),
+        control: CupertinoSwitch(
+          value: model.appThemeMode!=ThemeMode.light,
+          onChanged: (isOn) {
+            model.toggleAppThemeMode(isOn?ThemeMode.dark:ThemeMode.light);
+          },
+        ),
+        model: model,
+      ),
+      Row(
+        children: <Widget>[
+          ...(model.isDark?darkThemeList:themeList).map((Color color) {
+            return GestureDetector(
+              child: Container(
+                width: 40,
+                height: 40,
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: model.isDark?Colors.white:Colors.black),
+                  color: color
+                ),
+              ),
+              onTap: () {
+                model.changeTheme((model.isDark?darkThemeList:themeList).indexOf(color));
+              },
+            );
+          }).toList(),
+        ],
+      )
+    ];
+  }
+
+  Widget _renderItem({ Widget title, Widget control, GlobalModel model}) {
     return Container(
-      margin: EdgeInsets.only(top: 10),
+      margin: EdgeInsets.only(top: 20),
       padding: EdgeInsets.only(left: width(30), right: width(30)),
       height: 50,
       decoration: BoxDecoration(
-        color: isDark?Color(0xff666666):Color(0xfff9f9f9),
+        color: model.isDark?Color(0xff666666):Color(0xfff9f9f9),
         border: Border(
           top: BorderSide(color: Color(0xffcccccc), width: .5),
           bottom: BorderSide(color: Color(0xffcccccc), width: .5),
@@ -112,23 +175,5 @@ class SettingPageState extends State<SettingPage> {
         ],
       ),
     );
-  }
-
-  List<Widget> _renderList(handler) {
-    final List<Widget> list = [];
-    for(var i=0;i<themeList.length;i++) {
-      list.add(GestureDetector(
-        onTap: () {
-          handler(i);
-        },
-        child: Container(
-          width: 30,
-          height: 30,
-          margin: EdgeInsets.all(20),
-          color: themeList[i],
-        ),
-      ));
-    }
-    return list;
   }
 }
